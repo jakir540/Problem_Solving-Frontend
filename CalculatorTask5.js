fcd9312610ecded7d68ac55b7681ca8df1a860a6
jakir540 <jakirhossainmd540@gamil.com>
2023-07-27 09:22:11 +0600

    final check

--- CalculatorTask5.js
+++ CalculatorTask5.js
@@ -1,27 +1,25 @@
 //Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
 
-const Calculator =(num1,num2,operator) => {
-
-switch (operator) {
-    case '+':
-        return num1 + num2
-        break;
-    case '-':
-        return num1 - num2
-        break;
-    case '*':
-        return num1 * num2
-        break;
-    case '/':
-        return num1 / num2
-        break;
+const Calculator = (num1, num2, operator) => {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+      break;
+    case "-":
+      return num1 - num2;
+      break;
+    case "*":
+      return num1 * num2;
+      break;
+    case "/":
+      return num1 / num2;
+      break;
 
     default:
-        return "invalid operator"
-        break;
-}
+      return "invalid operator";
+      break;
+  }
+};
 
-}
-
-const result = Calculator(10,20,'+');
+const result = Calculator(10, 20, "+");
 console.log(result);
--- SecondSmallestNumArr.js
+++ SecondSmallestNumArr.js
@@ -1,12 +1,12 @@
 // Task 8: Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number
 
-const arr = [150, 120, 120, 180, 180, 910, 130, 60, 60];
+const arr = [150, 120, 120, 180, 180, 910, 130, 60];
 
 const secondSmallest = (arr) => {
   const sortArr = arr.sort((a, b) => {
     return a - b;
   });
-
+console.log(sortArr)
   let secondNumber;
   for (let i = 0; i < sortArr.length; i++) {
     
